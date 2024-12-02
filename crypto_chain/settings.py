a06ed231fe887715db9a8dbfbd30436b8898ddc4
Azeez Bello <azeezbello75@gmail.com>
2024-12-02 07:04:17 +0000

    heroku early deployment

--- crypto_chain/settings.py
+++ crypto_chain/settings.py
@@ -24,12 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mw$36&lvuxofzz*xp#z43j(bt2f^hcbf#b(r9sum-k3t3oq-xy'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-stayinblick-cryptochain-id2oz4dyuvk.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = [
+    '8000-stayinblick-cryptochain-id2oz4dyuvk.ws.codeinstitute-ide.net',
+    '.herokuapp.com'
+]
 
 
 # Application definition
